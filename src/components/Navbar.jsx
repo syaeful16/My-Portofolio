302556fff7af1ae06c666c216cdf8166806ef2e8
syaeful16 <syaefulanas121@gmail.com>
2023-02-22 16:43:36 +0700

    updt padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,14 +16,14 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='px-[2rem] lg:px-[6rem] xl:px-[12rem] py-4 flex justify-between'>
-      <h1 className='text-white font-montserrat font-bold text-xl md:text-2xl p-2'>ARNAS.</h1>
+    <nav className='px-[2rem] lg:px-[6rem] xl:px-[12rem] w-screen py-6 flex justify-between'>
+      <h1 className='text-white font-montserrat font-bold text-xl md:text-2xl'>ARNAS.</h1>
       <ul className='hidden font-poppins font-medium text-white md:flex'>
         {navMenu.map((menu, index) => (
           <li key={index} className={index === navMenu.length - 1 ? 'mr-0' : 'mr-8'}>{menu.name}</li>
         ))}
       </ul>
-      <div onClick={navHandle} className="block p-2 md:hidden">
+      <div onClick={navHandle} className="block md:hidden">
         { nav ? <AiOutlineClose size={20} color={'#ffffff'}/> : <AiOutlineMenu size={20} color={'#ffffff'}/> }
       </div>
     </nav>
