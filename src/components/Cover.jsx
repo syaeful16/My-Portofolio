afd295fca66493e1e0d5fad19ad77aaba113f873
Syaefulloh Anas <anazfingerstyle@gmail.com>
2023-02-23 03:48:13 +0000

    Update 3 files

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,38 +1,58 @@
-import React from 'react'
-import MyPhoto from '../assets/myPhoto2.png'
-import Typed from 'react-typed'
-import { HiArrowNarrowRight } from 'react-icons/hi';
+import React from "react";
+import MyPhoto from "../assets/myPhoto2.png";
+import Typed from "react-typed";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Cover = () => {
   return (
-    <div className="w-screen md:h-[90vh] h-max mt-8 md:mt-0 px-6 md:px-[12rem]">
+    <div className="w-screen lg:h-[90vh] h-max mt-8 md:mt-0 px-6 md:px-[8rem] 2xl:px-[12rem]">
       <div className="w-full h-full md:grid md:grid-cols-2">
         <div className="flex flex-col justify-center font-poppins">
-          <h2 className='text-2xl md:text-5xl font-medium text-[#A0A0A0] '>
+          <h2 className="text-3xl 2xl:text-5xl font-medium text-[#A0A0A0] ">
             <Typed
-                strings={['UI/UX Designer', 'Front End Developer']}
-                typeSpeed={140}
-                backSpeed={50}
-                loop
+              strings={["UI/UX Designer", "Front End Developer"]}
+              typeSpeed={140}
+              backSpeed={50}
+              loop
             />
           </h2>
-          <h1 className='text-4xl md:text-7xl font-bold text-white mt-2 md:mt-4'>Syaefulloh Arnas</h1>
-          <div className="bg-[#FFD568] w-[50%] md:w-[30%] h-[4px] md:h-[6px] rounded-lg my-4 md:my-8 lg:my-10"></div>
-          <p className='text-sm md:text-lg text-white leading-8 md:leading-10 font-light md:w-3/4'>During my college days I participated in several internal activities as a designer such as designing ui/ux and filling in Instagram content using <span className='font-bold'>Figma</span> and <span className='font-bold'>Adobe XD</span>. Besides that, I have made website projects with 
-            <span className='font-bold'> React js</span>, 
-            <span className='font-bold'> Laravel</span>, 
-            <span className='font-bold'> HTML</span>, 
-            <span className='font-bold'> PHP</span>, 
-            <span className='font-bold'> CSS</span>, 
-            <span className='font-bold'> JavaScript</span>, and 
-            <span className='font-bold'> Python</span>. If you want to get more information from me, you can contact me. Thank You</p>
-            <a className='text-[#FFD568] flex items-center gap-5 hover:gap-10 ease-in-out duration-200 text-lg md:text-xl mt-12 w-max' href="http://">Let's talk <HiArrowNarrowRight/></a>
+          <h1 className="text-4xl md:text-5xl 2xl:text-7xl font-bold text-white mt-2 md:mt-4">
+            Syaefulloh Arnas
+          </h1>
+          <div className="bg-[#FFD568] w-[50%] md:w-[40%] h-[4px] md:h-[6px] rounded-lg my-4 md:my-8 lg:my-10"></div>
+          <p className="text-sm md:text-md lg:text-lg text-white leading-8 md:leading-10 font-light md:w-3/4">
+            During my college days I participated in several internal activities
+            as a designer such as designing ui/ux and filling in Instagram
+            content using <span className="font-bold">Figma</span> and{" "}
+            <span className="font-bold">Adobe XD</span>. Besides that, I have
+            made website projects with
+            <span className="font-bold"> React js</span>,
+            <span className="font-bold"> Laravel</span>,
+            <span className="font-bold"> HTML</span>,
+            <span className="font-bold"> PHP</span>,
+            <span className="font-bold"> CSS</span>,
+            <span className="font-bold"> JavaScript</span>, and
+            <span className="font-bold"> Python</span>. If you want to get more
+            information from me, you can contact me. Thank You
+          </p>
+          <a
+            className="text-[#FFD568] flex items-center gap-5 hover:gap-10 ease-in-out duration-200 text-lg md:text-xl mt-12 w-max"
+            href="http://"
+          >
+            Let's talk <HiArrowNarrowRight />
+          </a>
         </div>
-        <div className='relative my-auto'>
-          <img src={MyPhoto} alt="" className='hidden md:block scale-[0.9]'/>
+        <div className="relative my-auto">
+          <img
+            src={MyPhoto}
+            alt=""
+            className="hidden md:block md:scale-[0.7] 2xl:scale-[0.9]"
+          />
           <div className="absolute bottom-20 right-0 hidden md:block">
-            <h2 className='text-white font-poppins text-lg'>Fresh Graduate</h2>
-            <h1 className='text-[#FFD568] font-poppins text-2xl'>S1 Teknik Informatika</h1>
+            <h2 className="text-white font-poppins text-lg">Fresh Graduate</h2>
+            <h1 className="text-[#FFD568] font-poppins text-2xl">
+              S1 Teknik Informatika
+            </h1>
           </div>
         </div>
       </div>
@@ -50,13 +70,13 @@ const Cover = () => {
     //       </h2>
     //       <h1 className='text-white text-4xl md:text-7xl font-bold mt-2 md:mt-4 font-poppins'>Syaefulloh Arnas</h1>
     //       <div className="bg-[#FFD568] w-[50%] md:w-[30%] h-[4px] md:h-[6px] rounded-lg my-4 md:my-8 lg:my-10"></div>
-    //       <p className='text-white font-poppins w-full lg:w-[72%] md:w-[82%] font-extralight leading-loose text-sm lg:text-lg lg:leading-10'>During my college days I participated in several internal activities as a designer such as designing ui/ux and filling in Instagram content using <span className='font-bold'>Figma</span> and <span className='font-bold'>Adobe XD</span>. Besides that, I have made website projects with 
-    //         <span className='font-bold'> React js</span>, 
-    //         <span className='font-bold'> Laravel</span>, 
-    //         <span className='font-bold'> HTML</span>, 
-    //         <span className='font-bold'> PHP</span>, 
-    //         <span className='font-bold'> CSS</span>, 
-    //         <span className='font-bold'> JavaScript</span>, and 
+    //       <p className='text-white font-poppins w-full lg:w-[72%] md:w-[82%] font-extralight leading-loose text-sm lg:text-lg lg:leading-10'>During my college days I participated in several internal activities as a designer such as designing ui/ux and filling in Instagram content using <span className='font-bold'>Figma</span> and <span className='font-bold'>Adobe XD</span>. Besides that, I have made website projects with
+    //         <span className='font-bold'> React js</span>,
+    //         <span className='font-bold'> Laravel</span>,
+    //         <span className='font-bold'> HTML</span>,
+    //         <span className='font-bold'> PHP</span>,
+    //         <span className='font-bold'> CSS</span>,
+    //         <span className='font-bold'> JavaScript</span>, and
     //         <span className='font-bold'> Python</span>. If you want to get more information from me, you can contact me. Thank You</p>
     //       <a className='text-[#FFD568] font-poppins flex items-center gap-5 hover:gap-10 ease-in-out duration-200 text-xl mt-12 w-max' href="http://">Let's talk <HiArrowNarrowRight/></a>
     //     </div>
@@ -69,7 +89,7 @@ const Cover = () => {
     //     </div>
     //   </div>
     // </div>
-  )
-}
+  );
+};
 
-export default Cover
+export default Cover;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,42 @@
-import React, { useState } from 'react'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const navMenu = [
-  {name: 'My Project', link: 'my-project'},
-  {name: 'Activity', link: 'activity'}
-]
-
+  { name: "My Project", link: "my-project" },
+  { name: "Activity", link: "activity" },
+];
 
 const Navbar = () => {
-
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
   const navHandle = () => {
     setNav(!nav);
-  }
+  };
 
   return (
-    <nav className='px-6 lg:px-[6rem] xl:px-[12rem] w-screen py-6 flex justify-between'>
-      <h1 className='text-white font-montserrat font-bold text-xl md:text-2xl'>ARNAS.</h1>
-      <ul className='hidden font-poppins font-medium text-white md:flex'>
+    <nav className="px-6 md:px-[8rem] 2xl:px-[12rem] w-screen py-6 flex justify-between">
+      <h1 className="text-white font-montserrat font-bold text-xl md:text-2xl">
+        ARNAS.
+      </h1>
+      <ul className="hidden font-poppins font-medium text-white md:flex">
         {navMenu.map((menu, index) => (
-          <li key={index} className={index === navMenu.length - 1 ? 'mr-0' : 'mr-8'}>{menu.name}</li>
+          <li
+            key={index}
+            className={index === navMenu.length - 1 ? "mr-0" : "mr-8"}
+          >
+            {menu.name}
+          </li>
         ))}
       </ul>
       <div onClick={navHandle} className="block md:hidden">
-        { nav ? <AiOutlineClose size={20} color={'#ffffff'}/> : <AiOutlineMenu size={20} color={'#ffffff'}/> }
+        {nav ? (
+          <AiOutlineClose size={20} color={"#ffffff"} />
+        ) : (
+          <AiOutlineMenu size={20} color={"#ffffff"} />
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
